bfa97e9a9a915ba5a8d81f2f343bb995044218b1
Sylivan Kenobi <sypsilon@hotmail.com>
2023-02-13 22:49:42 +0100

    cleanup repo user neopixelpanel as submodule

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
-import panel
-import font
-from neopixel_panel import NeopixelPanel
+import picoNeopixelMatrix.default_font as font
+from picoNeopixelMatrix.neopixel_panel import NeopixelPanel
 import colors
 import network
 import secrets
+from weather import Weather
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -22,29 +22,7 @@ rp2.PIO(0).remove_program()
 
 panel = NeopixelPanel(32, 8, 0, 28, "GRB")
 
-# t = time.localtime()
-# panel.display_string_at(0, 1, f'{t[3]}:{t[4]}', font, color)
+t = time.localtime()
+panel.display_string_at(0, 1, f'{t[3]}:{t[4]}', font, color)
 
-# black = (0,0,0)
-# time.sleep(2)
-# panel.clear()
-# panel.draw_horizontal_line(0, 2, 32, color)
-# 
-# 
-# panel.draw_vertical_line(20, 0, 8, color)
-
-for i in range(11):
-    panel.clear()
-    panel.display_string_at(0, 1, f'{i}', font, color)
-    panel.show()
-# 
-# 
-# panel.draw_rectangle(2, 2, 4,4, color)
-# 
-# 
-# panel.draw_filled_rectangle(0, 0, 16,4, color)
-# 
-# 
-# panel.draw_circle(18, 4, 3, color)
-time.sleep(10)
-panel.clear()
+panel.show()
--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-import requests
+import urequests
 
 class Weather:
 
